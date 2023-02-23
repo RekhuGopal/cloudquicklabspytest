5b1c55efbcb7d8c5535db47c53fbbf3d32ebce00
RekhuGopal <vrchinnarathod@gmail.com>
2023-02-23 20:58:56 +0530

    Ture test cases

--- AWSTestExecutions/test_iam.py
+++ AWSTestExecutions/test_iam.py
@@ -5,7 +5,7 @@ from AWSTestCases.AWS_IAM_Test import aws_iam_check_if_policy_exist, aws_iam_che
 iam = boto3.client('iam')
 
 def test_aws_iam_check_if_policy_exist():
-    assert aws_iam_check_if_policy_exist(iam) == False
+    assert aws_iam_check_if_policy_exist(iam) == True
 
 
 def test_aws_iam_check_if_role_exist():
