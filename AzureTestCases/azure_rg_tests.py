e6f14be034f752b0d61b1ee1bb76219ba1b61a44
RekhuGopal <vrchinnarathod@gmail.com>
2024-08-27 13:23:21 +0530

    Updated Pytest

--- AzureTestCases/azure_rg_tests.py
+++ AzureTestCases/azure_rg_tests.py
@@ -1,9 +1,9 @@
-def azure_check_if_rg_exists(resource_client, resource_group):
+def azure_check_if_rg_exists(resource_client, resource_group_name):
     try:
         # Retrieve the resource group
-        resource_group = resource_client.resource_groups.get(resource_group)
+        resource_group = resource_client.resource_groups.get(resource_group_name)
 
-        if resource_group.name == resource_group:
+        if resource_group.name == resource_group_name:
             return True
         else:
             return False
--- AzureTestCases/azure_storageaccount_tests.py
+++ AzureTestCases/azure_storageaccount_tests.py
@@ -1,7 +1,7 @@
 def azure_check_if_storage_account_exists (storage_client, resource_group, storage_account_name):
     try:
         # Retrieve the storage account
-        storage_account = storage_client.storage_accounts.get(resource_group, storage_account_name)
+        storage_account = storage_client.storage_accounts.get_properties(resource_group, storage_account_name)
 
         if storage_account.name == storage_account_name:
             return True
